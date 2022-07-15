a270b92203244860a6a57387d5499e3d7c28c4db
Andrew-M-C <laplacezhang@126.com>
2022-07-15 11:20:45 +0800

    [*] chore: alloc_test - 更新 jsonvalue 到版本 v1.3.0

--- alloc_test/benchmark_alloc_test.go
+++ alloc_test/benchmark_alloc_test.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"
 	"testing"
 
-	jsonvalue000 "github.com/Andrew-M-C/go.jsonvalue000"
+	jsonvalue "github.com/Andrew-M-C/go.jsonvalue"
 )
 
 // go test -bench=. -run=none -benchmem -benchtime=0.1s
@@ -12,98 +12,98 @@ import (
 func Benchmark_初始化一个_float(b *testing.B) {
 	raw := []byte("123.456")
 	for i := 0; i < b.N; i++ {
-		jsonvalue000.MustUnmarshalNoCopy(raw)
+		jsonvalue.MustUnmarshalNoCopy(raw)
 	}
 }
 
 func Benchmark_初始化一个_int(b *testing.B) {
 	raw := []byte("-123")
 	for i := 0; i < b.N; i++ {
-		jsonvalue000.MustUnmarshalNoCopy(raw)
+		jsonvalue.MustUnmarshalNoCopy(raw)
 	}
 }
 
 func Benchmark_初始化一个_uint(b *testing.B) {
 	raw := []byte("123")
 	for i := 0; i < b.N; i++ {
-		jsonvalue000.MustUnmarshalNoCopy(raw)
+		jsonvalue.MustUnmarshalNoCopy(raw)
 	}
 }
 
 func Benchmark_初始化一个_bool(b *testing.B) {
 	raw := []byte("true")
 	for i := 0; i < b.N; i++ {
-		jsonvalue000.MustUnmarshalNoCopy(raw)
+		jsonvalue.MustUnmarshalNoCopy(raw)
 	}
 }
 
 func Benchmark_初始化一个_array_空的(b *testing.B) {
 	raw := []byte(`[]`)
 	for i := 0; i < b.N; i++ {
-		jsonvalue000.MustUnmarshalNoCopy(raw)
+		jsonvalue.MustUnmarshalNoCopy(raw)
 	}
 }
 
 func Benchmark_初始化一个_array_1个元素(b *testing.B) {
 	raw := []byte(`[1]`)
 	for i := 0; i < b.N; i++ {
-		jsonvalue000.MustUnmarshalNoCopy(raw)
+		jsonvalue.MustUnmarshalNoCopy(raw)
 	}
 }
 
 func Benchmark_初始化一个_array_2个元素(b *testing.B) {
 	raw := []byte(`[1,1]`)
 	for i := 0; i < b.N; i++ {
-		jsonvalue000.MustUnmarshalNoCopy(raw)
+		jsonvalue.MustUnmarshalNoCopy(raw)
 	}
 }
 
 func Benchmark_初始化一个_array_32个元素(b *testing.B) {
 	raw := []byte(`[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]`)
 	for i := 0; i < b.N; i++ {
-		jsonvalue000.MustUnmarshalNoCopy(raw)
+		jsonvalue.MustUnmarshalNoCopy(raw)
 	}
 }
 
 func Benchmark_初始化一个_array_33个元素(b *testing.B) {
 	raw := []byte(`[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]`)
 	for i := 0; i < b.N; i++ {
-		jsonvalue000.MustUnmarshalNoCopy(raw)
+		jsonvalue.MustUnmarshalNoCopy(raw)
 	}
 }
 
 func Benchmark_初始化一个_array_34个元素(b *testing.B) {
 	raw := []byte(`[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]`)
 	for i := 0; i < b.N; i++ {
-		jsonvalue000.MustUnmarshalNoCopy(raw)
+		jsonvalue.MustUnmarshalNoCopy(raw)
 	}
 }
 
 func Benchmark_初始化一个_array_35个元素(b *testing.B) {
 	raw := []byte(`[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]`)
 	for i := 0; i < b.N; i++ {
-		jsonvalue000.MustUnmarshalNoCopy(raw)
+		jsonvalue.MustUnmarshalNoCopy(raw)
 	}
 }
 
 func Benchmark_初始化一个_object_空的(b *testing.B) {
 	raw := []byte(`{}`)
 	for i := 0; i < b.N; i++ {
-		jsonvalue000.MustUnmarshalNoCopy(raw)
+		jsonvalue.MustUnmarshalNoCopy(raw)
 	}
 }
 
 func Benchmark_初始化一个_object_1个元素(b *testing.B) {
 	raw := []byte(`{"1":1}`)
 	for i := 0; i < b.N; i++ {
-		jsonvalue000.MustUnmarshalNoCopy(raw)
+		jsonvalue.MustUnmarshalNoCopy(raw)
 	}
 }
 
 func Benchmark_初始化一个_object_2个元素(b *testing.B) {
 	raw := []byte(`{"1":1,"2":2}`)
 	for i := 0; i < b.N; i++ {
-		jsonvalue000.MustUnmarshalNoCopy(raw)
+		jsonvalue.MustUnmarshalNoCopy(raw)
 	}
 }
 
@@ -129,6 +129,6 @@ func Benchmark_初始化一个_object_2个元素_原生_json_struct(b *testing.B
 func Benchmark_初始化一个_string(b *testing.B) {
 	raw := []byte(`"string"`)
 	for i := 0; i < b.N; i++ {
-		jsonvalue000.MustUnmarshalNoCopy(raw)
+		jsonvalue.MustUnmarshalNoCopy(raw)
 	}
 }
--- alloc_test/go.mod
+++ alloc_test/go.mod
@@ -2,14 +2,4 @@ module github.com/Andrew-M-C/go.jsonvalue-test/alloc_test
 
 go 1.16
 
-require (
-	github.com/Andrew-M-C/go.jsonvalue v1.2.1 // indirect
-	github.com/Andrew-M-C/go.jsonvalue000 v0.0.0
-)
-
-replace (
-	github.com/Andrew-M-C/go.jsonvalue000 => ../../go.jsonvalue
-	github.com/Andrew-M-C/go.jsonvalue103 => github.com/Andrew-M-C/go.jsonvalue v1.0.3
-	github.com/Andrew-M-C/go.jsonvalue105 => github.com/Andrew-M-C/go.jsonvalue v1.0.5
-	github.com/Andrew-M-C/go.jsonvalue111 => github.com/Andrew-M-C/go.jsonvalue v1.1.1
-)
+require github.com/Andrew-M-C/go.jsonvalue v1.3.0
